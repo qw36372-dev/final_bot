9fc32d442ed2d1bb2ee618b50fa6637238c79a71
qw36372-dev <qw36372@gmail.com>
2026-02-09 11:35:49 +0300

    Add files via upload

---
+++ library/__init__.py
@@ -0,0 +1,65 @@
+"""
+library/__init__.py: Централизованный экспорт всех модулей библиотеки.
+Production-ready для использования в test_bot_main и specializations.
+"""
+
+# Базовые модели и enum
+from .enum import Difficulty
+from .models import Question, CurrentTestState
+from .states import TestStates
+
+# Загрузка вопросов
+from .question_loader import load_questions_for_specialization
+
+# Таймер
+from .timers import TestTimer, create_timer
+
+# Клавиатуры
+from .keyboards import (
+    get_main_keyboard,
+    get_difficulty_keyboard,
+    get_test_keyboard,
+    get_finish_keyboard
+)
+
+# Основная логика
+from .library import (
+    show_question,
+    handle_answer_toggle,
+    handle_next_question,
+    finish_test
+)
+
+# Middlewares
+from .middlewares import AntiSpamMiddleware, ErrorHandlerMiddleware
+
+__all__ = [
+    # Enum и модели
+    "Difficulty",
+    "Question",
+    "CurrentTestState",
+    "TestStates",
+    
+    # Загрузка вопросов
+    "load_questions_for_specialization",
+    
+    # Таймер
+    "TestTimer",
+    "create_timer",
+    
+    # Клавиатуры
+    "get_main_keyboard",
+    "get_difficulty_keyboard",
+    "get_test_keyboard",
+    "get_finish_keyboard",
+    
+    # Логика теста
+    "show_question",
+    "handle_answer_toggle",
+    "handle_next_question",
+    "finish_test",
+    
+    # Middlewares
+    "AntiSpamMiddleware",
+    "ErrorHandlerMiddleware",
+]
